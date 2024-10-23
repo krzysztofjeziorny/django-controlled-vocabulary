4e7af2f18823546c3e4fd79041622de8f6669b38
Krzysztof Jeziorny <git@jeziorny.net>
2024-10-23 13:43:22 +0200

    ditch Django 2.2, 3.2

--- tests/django2/tests/urls.py
+++
@@ -1,9 +0,0 @@
-from django.contrib import admin
-from django.urls import include, path
-
-
-urlpatterns = [
-    path("admin/", admin.site.urls),
-    # PATCH
-    path("vocabularies/", include("controlled_vocabulary.urls")),
-]
